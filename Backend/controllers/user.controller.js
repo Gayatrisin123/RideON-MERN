6fdaeabb096d6061cd2f540d12788a1f1fc412d4
Gayatrisin123 <150016577+Gayatrisin123@users.noreply.github.com>
2025-07-25 18:28:32 +0530

    updates

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -3,20 +3,24 @@
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
+const blackListTokenModel = require('../models/blacklistToken.model');
 
 
-module.exports.registerUSer = async (req, res,next) => {
+module.exports.registerUser = async (req, res,next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
  const { fullname, email, password } = req.body;
-
+ const isUserAlready = await userModel.findOne({ email });
+ if (isUserAlready) { 
+    return res.status(400).json({ message: 'User with this email already exists' });
+  }
 
   const hashedPassword = await userModel.hashPassword(password);
 
-  const user = await userService.createUSer({
+  const user = await userService.createUser({
     firstname : fullname.firstname,
     lastname : fullname.lastname,
     email,
@@ -44,8 +48,29 @@ module.exports.loginUser = async (req, res, next) => {
   if(!isMatch){
     return res.status(401).json({message : 'Ivalid email or password'});
   }
-
+ 
   const token = user.generateAuthToken();
+
+  res.cookie('token', token);
+  
   res.status(200).json({token, user});
+
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+
+  res.clearCookie('token');
+
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+  await blackListTokenModel.create({ token });
+  
+
+  res.status(200).json({message: 'Logged out successfully'});
+
 
 }
