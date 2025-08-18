8f0ebd1b662355f517e3ec74c8c5f033d442484d
Gayatrisin123 <150016577+Gayatrisin123@users.noreply.github.com>
2025-08-18 16:24:29 +0530

    login and signup pages

---
+++ frontend/src/pages/CaptainSignup.jsx
@@ -0,0 +1,76 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
+
+const CaptainSignup = () => {
+
+
+  const [email, setEmail] = useState('');
+      const [password, setPassword] = useState('');
+      const [firstName, setFirstName] = useState('');
+      const [lastName, setLastName] = useState('');
+      const [userData , setUserData] = useState({})
+    
+      const submitHandler = (e) => {
+        e.preventDefault();
+        setUserData({
+          fullName:{
+            firstName: firstName,
+            lastName: lastName,
+          },     
+          email: email,
+          password: password,
+        
+        });
+        
+        setEmail('');
+        setFirstName('');
+        setLastName('');
+        setPassword('');
+      }
+
+
+  return (
+    <div className = 'p-7 h-screen flex flex-col justify-between'>
+      <div><img className = "w-16 mb-10" src = "https://www.logo.wine/a/logo/Uber/Uber-Logo.wine.svg"></img>
+      <form onSubmit= {(e) => {submitHandler(e)}}>
+      <h3 className = 'text-lg w-full font-medium mb-2'>What's Our Captain name</h3>
+      <div className = "flex gap-2 mb-5">
+      <input required 
+      className = "bg-[#eeeeee] w-1/2  rounded px-4 py-2 border text-lg placeholder:text-base" type = "text" placeholder = "First Name"  
+      value = {firstName}
+      onChange = {(e) => setFirstName(e.target.value)}
+      ></input>
+
+      <input required 
+      className = "bg-[#eeeeee] w-1/2  rounded px-4 py-2 border  text-lg placeholder:text-base" type = "text" placeholder = "Last Name"
+      value = {lastName}
+      onChange = {(e) => setLastName(e.target.value)}
+      ></input>
+
+
+      </div>
+
+
+      <h3 className = 'text-lg font-medium mb-2'>What's our captains email</h3>
+      <input required 
+      className = "bg-[#eeeeee] mb-5 rounded px-4 py-2 border w-full text-base placeholder:text-base" type = "email" placeholder = "email@example.com"
+      value = {email}
+      onChange = {(e) => setEmail(e.target.value)}
+      ></input>
+      <h3 className = 'text-lg font-medium mb-2'>Enter Password</h3>
+      <input  className = "bg-[#eeeeee] mb-5 rounded px-4 py-2 border w-full text-base placeholder:text-base" required type = "password" placeholder = 'password'
+      value = {password}
+      onChange = {(e) => setPassword(e.target.value)}
+      ></input>
+      <button className = "bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 border w-full text-lg placeholder:text-base">Login</button>
+      </form>
+      <p className = "text-center" >Already have Account?<Link to = '/captain-login' className = "text-blue-600" >Login here</Link></p>
+      </div>
+      <div>
+       <p className = "text-[10px] leading-tight" >This site is proteected by recaptcha and the <span className = "underline">Google privacy policy</span> and terms of services</p>
+      </div>
+    </div>
+  )
+}
+
+export default CaptainSignup
